d08d7a4b60039dc8a429c3dcc4f11ea35921cdb8
bitshiftmod <bitshiftmod@gmail.com>
2024-10-28 00:00:04 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <div className="text-center">
+          <p className="font-bold text-5xl">&gt;&gt;%</p>
+          <p className="text-base">bitshiftmod</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <p>
+            Github:{" "}
+            <a href="https://github.com/bitshiftmod" rel="noreferrer">
+              @bitshiftmod
+            </a>
+          </p>
+          <p>
+            Twitter:{" "}
+            <a
+              href="https://twitter.com/bitshiftmod"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @bitshiftmod
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
